28d9dfd43b8dd5767290efee3dc133316e4b7e3c
tatar <tatarchuk2000@gmail.com>
2023-01-19 21:36:41 +0200

    Add admin panel

---
+++ models/Article.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "article".
+ *
+ * @property int $id
+ * @property string|null $title
+ * @property string|null $description
+ * @property string|null $content
+ * @property string|null $date
+ * @property string|null $image
+ * @property int|null $viewed
+ * @property int|null $category_id
+ * @property int|null $user_id
+ *
+ * @property Category $category
+ * @property Comment[] $comments
+ * @property User $user
+ */
+class Article extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'article';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['title'], 'required'],
+            [['description', 'content'], 'string'],
+            [['date'], 'date', 'format' => 'php:Y-m-d'],
+            [['date'], 'default', 'value' => date('Y-m-d')],
+            [['viewed', 'category_id', 'user_id'], 'integer'],
+            [['title', 'image'], 'string', 'max' => 255],
+            [['category_id'], 'exist', 'skipOnError' => true, 'targetClass' => Category::class, 'targetAttribute' => ['category_id' => 'id']],
+            [['user_id'], 'exist', 'skipOnError' => true, 'targetClass' => User::class, 'targetAttribute' => ['user_id' => 'id']],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'title' => 'Title',
+            'description' => 'Description',
+            'content' => 'Content',
+            'date' => 'Date',
+            'image' => 'Image',
+            'viewed' => 'Viewed',
+            'category_id' => 'Category ID',
+            'user_id' => 'User ID',
+        ];
+    }
+
+    /**
+     * Gets query for [[Category]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getCategory()
+    {
+        return $this->hasOne(Category::class, ['id' => 'category_id']);
+    }
+
+    /**
+     * Gets query for [[Comments]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getComments()
+    {
+        return $this->hasMany(Comment::class, ['article_id' => 'id']);
+    }
+
+    /**
+     * Gets query for [[User]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getUser()
+    {
+        return $this->hasOne(User::class, ['id' => 'user_id']);
+    }
+}
