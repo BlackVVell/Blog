6d439326fcc1e66659e18ebb6ced79d5a2161529
tatar <tatarchuk2000@gmail.com>
2023-01-21 11:25:26 +0200

    Update Registration

---
+++ views/site/signup.php
@@ -0,0 +1,52 @@
+<?php
+
+/** @var yii\web\View $this */
+/** @var yii\bootstrap5\ActiveForm $form */
+
+/** @var app\models\LoginForm $model */
+
+use yii\bootstrap5\ActiveForm;
+use yii\bootstrap5\Html;
+
+$this->title = 'Login';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="leave-comment mr-0">
+    <div class="row col-md-offset-2">
+
+        <div class="site-login">
+            <h1><?= Html::encode($this->title) ?></h1>
+
+            <p>Please fill out the following fields to login:</p>
+            <div class="col-md-3">
+                <?php $form = ActiveForm::begin([
+                    'id' => 'login-form',
+                    'layout' => 'horizontal',
+                    'fieldConfig' => [
+                        'template' => "{label}\n{input}\n{error}",
+                        'labelOptions' => ['class' => 'col-lg-1 col-form-label mr-lg-3'],
+                        'inputOptions' => ['class' => 'col-md-3 form-control'],
+                        'errorOptions' => ['class' => 'col-lg-7 invalid-feedback'],
+                    ],
+                ]); ?>
+                <?= $form->field($model, 'name')->textInput(['autofocus' => true]) ?>
+
+                <?= $form->field($model, 'login')->textInput() ?>
+
+                <?= $form->field($model, 'password')->passwordInput() ?>
+
+
+
+                <div class="form-group">
+                    <div class="offset-lg-1 col-lg-11">
+                        <?= Html::submitButton('Login', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
+                    </div>
+                </div>
+            </div>
+
+            <?php ActiveForm::end(); ?>
+
+        </div>
+
+    </div>
+</div>
